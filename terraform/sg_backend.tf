9f36cb9515705df81e69c767d70c992982c10507
Alex <alex.rodrigues23997@gmail.com>
2023-04-03 00:49:01 -0300

    Test webhook

--- terraform/sg_backend.tf
+++ terraform/sg_backend.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "SG_Backend" {
     cidr_blocks = ["${aws_instance.NginxServer.public_ip}/32", "${aws_instance.JenkinsServer.public_ip}/32", "186.216.72.206/32"]
   }
 
+  ingress {
+    from_port   = 8181
+    to_port     = 8181
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
